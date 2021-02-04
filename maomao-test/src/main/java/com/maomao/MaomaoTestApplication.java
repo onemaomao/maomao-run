c2bc420ed4498cd2c83890ce138bd86966127052
gujian <gujian@niwodai.net>
2021-02-04 16:56:56 +0800

    init

---
+++ maomao-test/src/main/java/com/maomao/MaomaoTestApplication.java
@@ -0,0 +1,13 @@
+package com.maomao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaomaoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaomaoTestApplication.class, args);
+	}
+
+}
---
+++ maomao-test/src/test/java/com/maomao/MaomaoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maomao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaomaoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
